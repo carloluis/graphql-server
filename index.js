d1d5c0377d13dd838c88fea9a02e8d970e4c2498
carloluis <carloluisr@gmail.com>
2016-12-18 01:13:35 -0300

    using graphQL's list type for collections

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ type Video {
 }
 
 type Query {
-	video: Video
+	video: Video,
+	videos: [Video]
 }
 
 type Schema {
@@ -19,18 +20,49 @@ type Schema {
 }
 `);
 
+const videoA = {
+	id: 'a',
+	title: 'Create a GrpahQL Schema',
+	duration: 256,
+	watched: true
+};
+
+const videoB = {
+	id: 'b',
+	title: 'Use GrpahQL Primitive Types',
+	duration: 117,
+	watched: true
+};
+
+const videoC = {
+	id: 'c',
+	title: 'Use GrpahQL\'s Object Type for Basic Types',
+	duration: 133,
+	watched: true
+};
+
+const videoD = {
+	id: 'd',
+	title: "Use GrpahQL's List Type for Collections",
+	duration: 145,
+	watched: false
+};
+
+const videos = [videoA, videoB, videoC, videoD];
+
 const resolvers = {
 	video: () => ({
 		id: '1',
 		title: 'movie',
 		duration: 180,
 		watched: true
-	})
+	}),
+	videos: () => videos
 };
 
 const query = `
 query myFirstQuery {
-	video {
+	videos {
 		id,
 		title,
 		duration,
